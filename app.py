e15b44d3d0423607e00bc9b38288b6b3dd3bb8a6
KekPeng <daniellovesblue@gmail.com>
2019-01-08 18:18:01 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "This is a homepage."
+
+@app.route("/profile/<name>")
+def profile(name):
+    return render_template("test.html", name=name)
+
+
+if __name__ == '__main__':
+    app.run()

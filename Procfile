d9f43cfc365a60e201a35762bed2902acbaedc49
KekPeng <daniellovesblue@gmail.com>
2019-01-08 19:21:28 +0800

    Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:profile
+web: gunicorn app:homepage
--- app.py
+++ app.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 def homepage():
     return "This is a homepage."
 
-@app.route("/profile/<name>")
-def profile(name):
-    return render_template("test.html", name=name)
+
 
 
 if __name__ == '__main__':
